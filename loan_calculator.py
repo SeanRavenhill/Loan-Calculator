115484b7d7592bebf2a3586f14228440ef33ab60
SeanRavenhill <seanravenhill@gmail.com>
2021-05-18 12:37:52 +0200

    implemented the ability for the user to exit/quit the program at any point into a function to reduce the lines of code and not repeat code unnecessarily

--- loan_calculator.py
+++ loan_calculator.py
@@ -1,18 +1,21 @@
 import math
 
+
+def exit_program(input):
+    if input.lower() == "done" or input.lower() == "exit":
+        exit()
+
+
 print("Enter the loan principal:")
 
 while True:
     principal = input()
-    if principal.lower() == "done" or principal.lower() == "exit":
-        exit()
-    else:
-        try:
-            principal = int(principal)
-            break
-        except ValueError:
-            print("Input Error. Please Try again.\n")
-
+    exit_program(principal)
+    try:
+        principal = int(principal)
+        break
+    except ValueError:
+        print("Input Error. Please Try again.\n")
 
 print("\nWhat do you want to calculate?")
 print('- type "m" - for number of monthly payments,')
@@ -20,9 +23,8 @@ print('- type "p" - for the monthly payment:\n')
 
 while True:
     calc = input()
-    if calc.lower() == "done" or calc.lower() == "exit":
-        exit()
-    elif calc == "m" or calc == "p":
+    exit_program(calc)
+    if calc == "m" or calc == "p":
         break
     else:
         print("Input Error. Please Try again.")
@@ -33,15 +35,13 @@ if calc == "m":
     print("Enter the monthly payment:")
     while True:
         m = input()
-        if m.lower() == "done" or m.lower() == "exit":
-            exit()
-        else:
-            try:
-                m = int(m)
-                months = int(round(principal / m, 0))
-                break
-            except ValueError:
-                print("Input Error. Please Try again.\n")
+        exit_program(m)
+        try:
+            m = int(m)
+            months = math.ceil(principal / m)
+            break
+        except ValueError:
+            print("Input Error. Please Try again.\n")
     if months == 1:
         print(f"\nIt will take {months} month to repay the loan\n")
     else:
@@ -50,17 +50,15 @@ else:
     print("Enter the number of months:")
     while True:
         p = input()
-        if p.lower() == "done" or p.lower() == "exit":
-            exit()
-        else:
-            try:
-                p = int(p)
-                pay = math.ceil(principal / p)
-                uneven_div = principal % p
-                last = principal - (pay * (p - 1))
-                break
-            except ValueError:
-                print("Input Error. Please Try again.\n")
+        exit_program(p)
+        try:
+            p = int(p)
+            pay = math.ceil(principal / p)
+            uneven_div = principal % p
+            last = principal - (pay * (p - 1))
+            break
+        except ValueError:
+            print("Input Error. Please Try again.\n")
     if uneven_div:
         print(f"Your monthly payment = {pay} and the last payment = {last}.")
     else:
