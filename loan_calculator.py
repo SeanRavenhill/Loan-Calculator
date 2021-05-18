431b4af1452694b45405bcf7ebe02fc147640de3
SeanRavenhill <seanravenhill@gmail.com>
2021-05-18 20:19:25 +0200

    trying to sort out an error catch for a 0% interest loan

--- loan_calculator.py
+++ loan_calculator.py
@@ -3,10 +3,126 @@ import math
 
 def exit_program(input):
     if input.lower() == "done" or input.lower() == "exit":
+        print("\nProgram shutting down.\n")
         exit()
+    else:
+        pass
 
 
-print("Enter the loan principal:")
+def monthly_payments():
+    while True:
+        print("\nEnter the loan principal:")
+        loan = input()
+        exit_program(loan)
+        try:
+            loan = int(loan)
+            break
+        except ValueError:
+            print("\nInput Error. Please Try again.")
+
+    while True:
+        print("\nEnter the monthly payment:")
+        payment = input()
+        exit_program(payment)
+        try:
+            payment = int(payment)
+            break
+        except ValueError:
+            print("\nInput Error. Please Try again.")
+
+    while True:
+        print("\nEnter the loan interest:")
+        interest = input()
+        exit_program(interest)
+        if interest == 0:
+            print("\nSorry calculator does not work with 0% interest loands.")
+            monthly_payments()
+        else:
+            try:
+                interest = int(interest)
+                break
+            except ValueError:
+                print("\nInput Error. Please Try again.")
+
+    rate = interest / (12 * 100)
+    months = math.ceil(math.log((payment / (payment - rate * loan)), 1 + rate))
+    y = math.floor(months / 12)
+    m = months % 12
+
+    if y >= 2 and m >= 2:
+        print(f"\nIt will take {y} years and {m} months to repay this loan!")
+    elif y == 1 and m >= 2:
+        print(f"\nIt will take {y} year and {m} months to repay this loan!")
+    elif y >= 2 and m == 1:
+        print(f"\nIt will take {y} years and {m} month to repay this loan!")
+    elif y == 0 and m >= 2:
+        print(f"\nIt will take {m} months to repay this loan!")
+    elif y == 0 and m == 1:
+        print(f"\nIt will take {m} month to repay this loan!")
+    elif y >= 2 and m == 0:
+        print(f"\nIt will take {y} years to repay this loan!")
+    elif y == 1 and m == 0:
+        print(f"\nIt will take {y} year to repay this loan!")
+
+    # if months == 1:
+    # print(f"\nIt will take {months} month to repay the loan\n")
+    # else:
+    # print(f"\nIt will take {months} months to repay the loan\n")
+
+
+def annuity():
+    print("\nEnter the loan principal:")
+    a = input()
+    exit_program(a)
+
+
+def principal():
+    print("\nEnter the loan principal:")
+    p = input()
+    exit_program(p)
+
+
+print("\nWhat do you want to calculate?")
+print('\n- type "n" for number of monthly payments,')
+print('- type "a" for annuity monthly payment amount,')
+print('- type "p" for loan principal:')
+
+while True:
+    calc = input().lower()
+    exit_program(calc)
+    if calc == "n" or calc == "a" or calc == "p":
+        break
+    else:
+        print("\nInput Error. Please Try again.")
+        print('\n- type "n" for number of monthly payments,')
+        print('- type "a" for annuity monthly payment amount,')
+        print('- type "p" for loan principal:')
+
+if calc == "n":
+    monthly_payments()
+elif calc == "a":
+    annuity()
+elif calc == "p":
+    principal()
+else:
+    print("\nProgram Error. Shutting down.\n")
+
+
+'''
+    while True:
+        n = input()
+        exit_program(n)
+        try:
+            m = int(m)
+            months = math.ceil(principal / m)
+            break
+        except ValueError:
+            print("Input Error. Please Try again.\n")
+    if months == 1:
+        print(f"\nIt will take {months} month to repay the loan\n")
+    else:
+        print(f"\nIt will take {months} months to repay the loan\n")
+
 
 while True:
     principal = input()
@@ -16,10 +132,6 @@ while True:
         break
     except ValueError:
         print("Input Error. Please Try again.\n")
-
-print("\nWhat do you want to calculate?")
-print('- type "m" - for number of monthly payments,')
-print('- type "p" - for the monthly payment:\n')
 
 while True:
     calc = input()
@@ -63,3 +175,4 @@ else:
         print(f"Your monthly payment = {pay} and the last payment = {last}.")
     else:
         print(f"Your monthly payment = {pay}")
+'''
